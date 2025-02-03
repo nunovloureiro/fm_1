a9b4fb819ca6135ddad1abe6643e2fc0d9406ba6
nunovloureiro <nunovloureiro@gmail.com>
2025-02-03 20:44:30 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,19 @@
 let bg = 0;
 let start = 0;
+let webpdReady = false;
 
 
 let i_rx = 0;
 let rx = 0;
 let ry = 0;
 let rz = 0;
+
+
+window.addEventListener("WebPdReady", () => {
+    webpdReady = true;
+    console.log("✅ WebPd is ready! Sending messages can start.");
+});
+
 
 function setup() {
     background(100);
@@ -42,47 +50,49 @@ function handleOrientation(event) {
 
     rotationValueLimiter();
 }
-    
+
 
 
 function draw() {
 
     i_rx = map(rotationX, 0, HALF_PI, 0, 1, false);
     rx = parseFloat(i_rx).toPrecision(2);
-    
+
     i_ry = map(rotationY, 0, HALF_PI, 0, 1, false);
     ry = parseFloat(i_ry).toPrecision(2);
-    
+
     i_rz = map(rotationZ, 0, HALF_PI, 0, 1, true);
     rz = parseFloat(i_rz).toPrecision(2);
-  
+
     rotationValueLimiter();
-  
+
     background(0,0,0);
     noStroke();
-    
+
     fill(255, 0, 0);
     rect(window.innerWidth / 4, window.innerHeight / 2, 5, 0 + rx * 100);
-  
+
     fill(0,255,0);
     //rect(window.innerWidth/2, window.innerHeight/2, 5, 0 + ry*100);
-  
+
     fill(0,0,255);
     //rect(window.innerWidth-window.innerWidth/4, window.innerHeight/2, 5, 0 + rz*100);
-    
+
     textAlign(LEFT);
     text('rx: ' + rx, 20, 40);
     text('ry: ' + ry, 20, 60);
-    text('rz: ' + rz, 20, 80); 
-    text('mX: ' + mouseX, 20, 100); 
+    text('rz: ' + rz, 20, 80);
+    text('mX: ' + mouseX, 20, 100);
 
 
-    
+
     if (start = 0) {
         textAlign(CENTER);
         fill(255, 255, 0);
         text('click to start audio', width / 2, height / 2);
     }
+
+
 
         // // Dispatch the event with the latest values of rx, ry, rz
         const motionDataEvent = new CustomEvent('motionData', {
@@ -93,6 +103,13 @@ function draw() {
             }
         });
         window.dispatchEvent(motionDataEvent);
+
+        if (typeof webpdNode !== 'undefined' && webpdNode !== null) {
+            sendMsgToWebPd('n_0_5', '0', [rx]);  // Ensure WebPd is ready before sending
+        } else {
+            console.warn("WebPd is not ready yet. Skipping sendMsgToWebPd call.");
+        }
+        
 }
 
 function mousePressed(){
@@ -116,5 +133,3 @@ function createMetaTag() {
     let head = select('head');
     meta.parent(head);
 }
-
-
