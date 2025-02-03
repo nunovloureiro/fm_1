776de56a592e374bcf169d5c56c5108901cf429a
nunovloureiro <nunovloureiro@gmail.com>
2025-02-03 17:40:26 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,18 +15,18 @@ function setup() {
     rectMode(CENTER);
 
      // Request permission for device orientation on iOS
-     if (typeof DeviceOrientationEvent.requestPermission === 'function') {
-        DeviceOrientationEvent.requestPermission()
-            .then(permissionState => {
-                if (permissionState === 'granted') {
-                    window.addEventListener('deviceorientation', handleOrientation);
-                }
-            })
-            .catch(console.error);
-    } else {
-        // Handle regular non-iOS devices
-        window.addEventListener('deviceorientation', handleOrientation);
-    }
+  //    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+  //       DeviceOrientationEvent.requestPermission()
+  //           .then(permissionState => {
+     //            if (permissionState === 'granted') {
+        //             window.addEventListener('deviceorientation', handleOrientation);
+           //      }
+             //})
+  //           .catch(console.error);
+    // } else {
+      //   // Handle regular non-iOS devices
+        // window.addEventListener('deviceorientation', handleOrientation);
+   //  }
 }
 
 // function handleOrientation(event) {
