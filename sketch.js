31b5aed0fdd5b873be032097d164dc8c2b8e4c96
nunovloureiro <nunovloureiro@gmail.com>
2025-02-12 19:02:27 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -33,7 +33,7 @@ let requestButton = 0;
 let androidStop = 0;
 
 
-let initTouch = 0;
+let inittouch. = 0;
 
 // let iosSensorPermissionRequested = false;
 
@@ -200,7 +200,7 @@ function gui(){
 function touchStarted(){
 
     //play screen  && requestButton == 1
-      if (go == 0 && touchX > window.innerWidth/2 - TWstartText/2 && touchX < window.innerWidth/2 + TWstartText/2 && touchY > window.innerHeight/2 - textHeight && touchY < window.innerHeight/2 + textHeight){
+      if (go == 0 && touch.X > window.innerWidth/2 - TWstartText/2 && touch.X < window.innerWidth/2 + TWstartText/2 && touch.Y > window.innerHeight/2 - textHeight && touch.Y < window.innerHeight/2 + textHeight){
         startApp(); //starts webPD audio app
         checkAppStart();
         console.log('WebPD started supostamente');
@@ -208,20 +208,20 @@ function touchStarted(){
       }
     
     //menu enter
-      if (go == 1 && menu == 0 && keyMenu == 0 && touchX > window.innerWidth/2 - window.innerWidth/10 && touchX < window.innerWidth/2 + window.innerWidth/10 && touchY > window.innerHeight/2 - textHeight && touchY < window.innerHeight/2 + textHeight ){
+      if (go == 1 && menu == 0 && keyMenu == 0 && touch.X > window.innerWidth/2 - window.innerWidth/10 && touch.X < window.innerWidth/2 + window.innerWidth/10 && touch.Y > window.innerHeight/2 - textHeight && touch.Y < window.innerHeight/2 + textHeight ){
         menu = 1;
         return;
       }
     
     //keyMenu enter
-     if (go == 1 && menu == 1 && keyMenu == 0 && touchX > window.innerWidth/2 - window.innerWidth/10 && touchX < window.innerWidth/2 + window.innerWidth/10 && touchY > window.innerHeight/2 - window.innerHeight/8 - textHeight && touchY < window.innerHeight/2 - window.innerHeight/8 + textHeight){
+     if (go == 1 && menu == 1 && keyMenu == 0 && touch.X > window.innerWidth/2 - window.innerWidth/10 && touch.X < window.innerWidth/2 + window.innerWidth/10 && touch.Y > window.innerHeight/2 - window.innerHeight/8 - textHeight && touch.Y < window.innerHeight/2 - window.innerHeight/8 + textHeight){
         menu = 0;
         keyMenu = 1;
         return;
      }
 
     //menu exit to play screen
-    if (go == 1 && menu == 1 && keyMenu == 0 && touchX > window.innerWidth/2 - window.innerWidth/8 && touchX < window.innerWidth/2 + window.innerWidth/8 && touchY > window.innerHeight/2 + window.innerHeight/8 + textHeight/2 && touchY < window.innerHeight/2 + window.innerHeight/8 + 2*textHeight){
+    if (go == 1 && menu == 1 && keyMenu == 0 && touch.X > window.innerWidth/2 - window.innerWidth/8 && touch.X < window.innerWidth/2 + window.innerWidth/8 && touch.Y > window.innerHeight/2 + window.innerHeight/8 + textHeight/2 && touch.Y < window.innerHeight/2 + window.innerHeight/8 + 2*textHeight){
         menu = 0;
         go = 1;
         keyMenu = 0;
@@ -272,7 +272,7 @@ function touchStarted(){
       
       
       //exit to play screen
-      if(touchX > window.innerWidth/2 - window.innerWidth/10 && touchX < window.innerWidth/2 + window.innerWidth/10 && touchY > window.innerHeight - 2 * window.innerHeight/10 && touchY < window.innerHeight + 2 * window.innerHeight/10) {
+      if(touch.X > window.innerWidth/2 - window.innerWidth/10 && touch.X < window.innerWidth/2 + window.innerWidth/10 && touch.Y > window.innerHeight - 2 * window.innerHeight/10 && touch.Y < window.innerHeight + 2 * window.innerHeight/10) {
         keyMenu = 0;
         menu = 0;
         go = 1;
@@ -313,20 +313,20 @@ function initKeyboard(){
   }
 
 
-//function touchStarted() {
+//function touch.Started() {
 
-    // if (initTouch == 0){
+    // if (inittouch. == 0){
     //    startApp();
-    //    initTouch = 1;
+    //    inittouch. = 1;
     // }
-  //On the very first touch, request iOS motion sensor permission
+  //On the very first touch., request iOS motion sensor permission
   // if (!iosSensorPermissionRequested) {
   //   iosSensorPermissionRequested = true;
     // requestMotionSensorPermission();
   //}
 
-  // Continue with your normal touch behavior
-  //touchPressed();
+  // Continue with your normal touch. behavior
+  //touch.Pressed();
 //}
 
 
