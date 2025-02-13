bfd46e2813fbd201aad8cc3546f34ce6f8625671
nunovloureiro <nunovloureiro@gmail.com>
2025-02-13 16:04:56 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -10,6 +10,7 @@ document.addEventListener('touchstart', {});
 
 let mySketch = function(p) {
 
+let p.kbKey = kbKey;
 
 let mx;
 let my;
@@ -424,36 +425,34 @@ p.showKeyboard = function() {
   // }
 
 
-// p.kbKey = class {
-//     constructor(p, x, y, keySize, keyboardSW, w_b, keyID) {   //   w_b -> white or black key
-//       this.p = p;
-//       this.x = x;
-//       this.y = y;
-//       this.keySize = keySize;
-//       this.keySW = keyboardSW;
-//       this.keyID = keyID;
-//       this.w_b = w_b;
-//       //this.selected = keySelector;
-//       this.selected = 255;
-//     }
-    
-//     show = function() {  
-//       this.p.strokeWeight(this.keySW);
-//       this.p.stroke(0);
-//       this.p.fill(0,0,this.selected);
-//       this.p.square(this.x, this.y,  this.keySize);
-//     }
-    
-    
-//     p.isClicked = function(){
-//       let offsetY = this.w_b === 1 ? this.keySize * 0.3 : 0; // Slightly shift detection for black keys
-//       return (p.mouseX > this.x - this.keySize / 2 &&
-//         this.p.mouseX < this.x + this.keySize / 2 &&
-//         this.p.mouseY > this.y - this.keySize / 2 &&
-//         this.p.mouseY < this.y + this.keySize / 2);
-//     }
-//   }
+  p.kbKey = class {
+    constructor(p, x, y, keySize, keyboardSW, w_b, keyID) {
+      this.p = p;
+      this.x = x;
+      this.y = y;
+      this.keySize = keySize;
+      this.keySW = keyboardSW;
+      this.keyID = keyID;
+      this.w_b = w_b;
+      this.selected = 255;
+    }
 
+    show() {  
+      this.p.strokeWeight(this.keySW);
+      this.p.stroke(0);
+      this.p.fill(0, 0, this.selected);
+      this.p.square(this.x, this.y, this.keySize);
+    }
+
+    isClicked() {
+      return (
+        this.p.mouseX > this.x - this.keySize / 2 &&
+        this.p.mouseX < this.x + this.keySize / 2 &&
+        this.p.mouseY > this.y - this.keySize / 2 &&
+        this.p.mouseY < this.y + this.keySize / 2
+      );
+    }
+  };
 
 }  
 
